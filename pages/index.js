367141527c2e5aa95f0f2eb2ab9b5199bc1c0658
jmartinez15 <jeffrey.martinez@logdna.com>
2021-09-07 13:39:14 -0700

    Changed Title

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Testing and Creating a Blog with Next.js using Vercel and logging with LogDNA.</p>
+        <p>How to configure Vercel and LogDNA.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
